c621c97218f91730d4567560a87ac78b94ce481f
Elorfin <penin.axel@gmail.com>
2014-01-09 11:03:22 +0100

    first commit

---
+++ InnovaAngularUITinyMCEBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Innova\InnovaAngularUITinyMCEBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+
+class InnovaAngularUITinyMCEBundle extends Bundle implements AutoConfigurableInterface
+{
+    public function supports($environment)
+    {
+        return true;
+    }
+    
+    public function getConfiguration($environment)
+    {
+        return new ConfigurationBuilder();
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# AngularUITinyMCEBundle
+
+Symfony 2 bundle which provides Angular JS directive to integrate TinyMCE .
+
+## Installation
+
+Install with composer :
+	
+	$ composer require "innova/angular-ui-tinymce-bundle" "1.0"
+
+## Authors
+
+* Donovan Tengblad (purpplefish32)
+* Axel Penin (Elorfin)
+* Arnaud Bay (arnaudbey)
+* Eric Vincent (ericvincenterv)
