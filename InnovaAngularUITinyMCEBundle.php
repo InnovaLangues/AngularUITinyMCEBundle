1d37e1f51883e5e2c456e10f8506a8bbe8f308e0
Elorfin <penin.axel@gmail.com>
2015-05-12 14:35:25 +0200

    upgrade + remove dependency to claroline

--- InnovaAngularUITinyMCEBundle.php
+++ InnovaAngularUITinyMCEBundle.php
@@ -3,18 +3,8 @@
 namespace Innova\AngularUITinyMCEBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
-use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class InnovaAngularUITinyMCEBundle extends Bundle implements AutoConfigurableInterface
+class InnovaAngularUITinyMCEBundle extends Bundle
 {
-    public function supports($environment)
-    {
-        return true;
-    }
-    
-    public function getConfiguration($environment)
-    {
-        return new ConfigurationBuilder();
-    }
+
 }
